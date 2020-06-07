842a55a9c95a2ccd66bea25cc40a745e802b9025
antonnifo <antonnifo@live.com>
2020-06-07 11:36:45 +0300

    we set the order ID in the current session using the order_id session key

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,6 @@
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 from cart.cart import Cart
 
@@ -25,9 +26,10 @@ def order_create(request):
 
         # launch asynchronous task
         order_created.delay(order.id)
-        return render(request,
-                    'orders/order/created.html',
-                    {'order': order})
+        # set the order in the session
+        request.session['order_id'] = order.id
+        # redirect for payment
+        return redirect(reverse('payment:process'))
     else:
         form = OrderCreateForm()
     return render(request,
